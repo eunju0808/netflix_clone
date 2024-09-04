a48277cd6b61527669383a7319964f72586659bd
eunju0808 <juju_dyu@naver.com>
2024-09-04 17:31:58 +0900

    0904

--- js/main.js
+++ js/main.js
@@ -1,11 +1,8 @@
 $(function(){
-    
-
     $(".list>li>a").click(function(){
         $(this).toggleClass('on').parent().siblings().find('a').removeClass('on')
         $(this).addClass("on")
         $(this).next().slideToggle().parent().siblings()
         .find('.depth2').slideUp();
         })
-
 })
